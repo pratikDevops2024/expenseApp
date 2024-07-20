a332e9d41ef8104868cd7ca266802e584fab325b
Pratik <pratik.kumbhar2017@gmail.com>
2024-07-20 16:19:09 +0530

    Added the shell script for setting the frontend of the application

---
+++ Iteration-1/frontEnd.sh
@@ -0,0 +1,24 @@
+#Switch to root User
+sudo -i
+
+#Install Nginx
+dnf install nginx -y
+
+#Start & Enable Nginx service
+systemctl enable nginx
+systemctl start nginx
+
+#Remove the default content that web server is serving.
+rm -rf /usr/share/nginx/html/*
+
+#Download the frontend content
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+#Extract the frontend content.
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+#Create Nginx Reverse Proxy Configuration.
+vim /etc/nginx/default.d/expense.conf
+
+systemctl restart nginx 
