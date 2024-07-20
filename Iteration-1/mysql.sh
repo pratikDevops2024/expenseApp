ea31e0ba0d29a14ebd36632f21ebedd7d04d59ee
Pratik <pratik.kumbhar2017@gmail.com>
2024-07-20 23:25:59 +0530

    Updated the sql installation script

--- Iteration-1/mysql.sh
+++ Iteration-1/mysql.sh
@@ -6,4 +6,4 @@ systemctl enable mysqld
 systemctl start mysqld
 
 #Set the root User password
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass "ExpenseApp@1"
--- Iteration-2/mysql.sh
+++ Iteration-2/mysql.sh
@@ -1,9 +1,37 @@
+source common.sh
+
+TIMESTAMP=$(date '+%Y%m%d_%H%M%S')
+LOG_FILE="/tmp/mysql_$TIMESTAMP.log"
+
 #Install the Mysql Server
-dnf install mysql-server -y
+HEADING Installing Mysql Server
+dnf install mysql-server -y $>>"$LOG_FILE"
+STAT $?
 
 #Enable and Start the Mysql Server
-systemctl enable mysqld
-systemctl start mysqld
+HEADING Enable and Start the Mysql Server
+systemctl enable mysqld $>>"$LOG_FILE"
+systemctl start mysqld $>>"$LOG_FILE"
+STAT $?
+
+# Set the root User password
+HEADING Setting the root User password
+mysqladmin password 'ExpenseApp@1' || true  # Set root password with mysqladmin (suppress errors)
+
 
 #Set the root User password
+HEADING Set the root User password
 mysql_secure_installation --set-root-pass ExpenseApp@1
+STAT $?
+
+# Run mysql_secure_installation to secure MySQL installation
+HEADING Running mysql_secure_installation to secure MySQL installation
+mysql_secure_installation --set-root-pass 'ExpenseApp@1' <<EOF
+
+y  # Remove anonymous users
+n  # Disallow root login remotely
+y  # Remove test database and access to it
+y  # Reload privilege tables now
+
+EOF
+STAT $?
