a1da8f221886e5f49c93e5289d7b656f8d995b14
Pratik <pratik.kumbhar2017@gmail.com>
2024-07-20 23:28:09 +0530

    Updated the sql installation script

--- Iteration-2/mysql.sh
+++ Iteration-2/mysql.sh
@@ -5,23 +5,18 @@ LOG_FILE="/tmp/mysql_$TIMESTAMP.log"
 
 #Install the Mysql Server
 HEADING Installing Mysql Server
-dnf install mysql-server -y $>>"$LOG_FILE"
+dnf install mysql-server -y &>>"$LOG_FILE"
 STAT $?
 
 #Enable and Start the Mysql Server
 HEADING Enable and Start the Mysql Server
-systemctl enable mysqld $>>"$LOG_FILE"
-systemctl start mysqld $>>"$LOG_FILE"
+systemctl enable mysqld &>>"$LOG_FILE"
+systemctl start mysqld &>>"$LOG_FILE"
 STAT $?
 
 # Set the root User password
 HEADING Setting the root User password
 mysqladmin password 'ExpenseApp@1' || true  # Set root password with mysqladmin (suppress errors)
-
-
-#Set the root User password
-HEADING Set the root User password
-mysql_secure_installation --set-root-pass ExpenseApp@1
 STAT $?
 
 # Run mysql_secure_installation to secure MySQL installation
